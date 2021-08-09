91c69802fd5120c7df8e2e440898a515daf89495
Akunne <linar@linars-MacBook-Pro.local>
2021-08-09 22:06:55 +0100

    installed php storm

--- test/controllers/BaseControllerTest.php
+++ test/controllers/BaseControllerTest.php
@@ -2,7 +2,7 @@
 
 namespace App\Tests;
 
-class BaseController
+class BaseControllerTest
 {
     
 }
--- test/controllers/BookControllerTest.php
+++ test/controllers/BookControllerTest.php
@@ -2,7 +2,7 @@
 
 namespace App\Tests;
 
-class BookController
+class BookControllerTest
 {
-    
+
 }
